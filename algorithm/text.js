73604f8a56c65ef09a933d6c8df585fcb5a522ac
Simon Veith <simon@jinfinote.com>
2011-01-08 18:15:29 +0100

    Added README and LICENSE files; updated copyright year

--- algorithm/text.js
+++ algorithm/text.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2009 Simon Veith <simon@jinfinote.com>
+Copyright (c) 2009-2011 Simon Veith <simon@jinfinote.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
